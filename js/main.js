491df761db7028ed1330a1048bd74f6798806d8c
frontWeb-dev <101559564+frontWeb-dev@users.noreply.github.com>
2023-11-26 21:40:31 +0900

    :sparkles: Scroll hight setting

---
+++ js/main.js
@@ -0,0 +1,53 @@
+(() => {
+  const sceneInfo = [
+    {
+      // scroll-section-0
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-0"),
+      },
+    },
+    {
+      // scroll-section-1
+      type: "normal",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-1"),
+      },
+    },
+    {
+      // scroll-section-2
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-2"),
+      },
+    },
+    {
+      // scroll-section-3
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-3"),
+      },
+    },
+  ];
+
+  // 각 section별 scroll Hight 설정
+  const setLayout = () => {
+    for (let i = 0; i < sceneInfo.length; i++) {
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }
+  };
+
+  // 브라우저 창 크기가 변경될 때마다 재실행
+  window.addEventListener("resize", setLayout);
+
+  setLayout();
+})();
